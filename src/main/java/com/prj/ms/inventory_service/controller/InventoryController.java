882033d3a8b8213d84ac5637a82d748fc39fde3e
AyushMishra10 <iamayushmishra28@gmail.com>
2024-09-29 15:14:57 +0530

    Completed Inventory Service Initial Development

---
+++ src/main/java/com/prj/ms/inventory_service/controller/InventoryController.java
@@ -0,0 +1,27 @@
+package com.prj.ms.inventory_service.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.prj.ms.inventory_service.service.InventoryService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class InventoryController {
+	
+	private final InventoryService inventoryService;
+	
+	@GetMapping("/inStock")
+	@ResponseStatus(HttpStatus.OK)
+	public boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity) {
+		return inventoryService.isInStock(skuCode, quantity);
+	}
+
+}
